9ce4da3ae846b04d60ef533ac608f65f1919efde
Levi Lansing <helives@gmail.com>
2019-05-10 18:49:15 -0400

    fix test due to configuration change

--- src/Api/config/configure.test.ts
+++ src/Api/config/configure.test.ts
@@ -2,7 +2,7 @@ import 'jest';
 import {ApiV3} from '../lib/ApiV3';
 import {config, configure} from './configure';
 
-jest.mock('./ApiV3');
+jest.mock('../lib/ApiV3');
 
 describe('configure', () => {
   it('configures the api module', () => {
@@ -14,7 +14,12 @@ describe('configure', () => {
 
     configure(configuration);
 
-    expect(ApiV3.configure).toHaveBeenCalledWith(configuration);
+    // set by setup.ts
+    const defaultConfig = {
+      requestId: '00000000-0000-0000-0000-000000000000',
+      privateApiKey: 'private_api_key'
+    };
+    expect(ApiV3.configure).toHaveBeenCalledWith(Object.assign(defaultConfig, configuration));
   });
 
   it('exposes the tracker id', () => {
