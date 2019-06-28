ecf2fa09735f6853adfd0e134e5358b024fd6814
mejmt <john@zaius.com>
2019-06-28 11:23:00 -0400

    ZAIUS-8112: upload customers

---
+++ src/Api/Customers/customer.test.ts
@@ -0,0 +1,47 @@
+import {ApiV3} from '../lib/ApiV3';
+import {Customer} from '../Types';
+import {customer} from './customer';
+
+describe('customer', () => {
+  let postMock!: jest.Mock;
+  beforeEach(() => {
+    postMock = jest.spyOn(ApiV3, 'post').mockReturnValue(Promise.resolve({} as any));
+  });
+
+  it('sends a transformed post with one customer to /profiles', async () => {
+    const payload: Customer = {identifiers: {email: 'test@zaius.com'}, attributes: {name: 'Jim Bob'}};
+    const transformedPayload = {attributes: {name: 'Jim Bob', email: 'test@zaius.com'}};
+    await customer(payload);
+    expect(postMock).toHaveBeenCalledWith('/profiles', transformedPayload);
+  });
+
+  it('sends a transformed post with several customers to /profiles', async () => {
+    const payload: Customer[] = [
+      {identifiers: {email: 'test1@zaius.com'}, attributes: {name: 'Jim Bob'}},
+      {identifiers: {email: 'test2@zaius.com'}, attributes: {name: 'Bob Joe'}},
+      {identifiers: {email: 'test3@zaius.com'}, attributes: {name: 'Joe Jim'}}
+    ];
+    const transformedPayload = [
+      {attributes: {name: 'Jim Bob', email: 'test1@zaius.com'}},
+      {attributes: {name: 'Bob Joe', email: 'test2@zaius.com'}},
+      {attributes: {name: 'Joe Jim', email: 'test3@zaius.com'}}
+    ];
+    await customer(payload);
+    expect(postMock).toHaveBeenCalledWith('/profiles', transformedPayload);
+  });
+
+  it('throws an error if too many customers are sent in one call', async () => {
+    const payload: Customer[] = [];
+    for (let i = 0; i < 501; i++) {
+      payload.push({identifiers: {email: 'test@zaius.com'}, attributes: {name: 'Jim Bob'}});
+    }
+
+    expect.assertions(2);
+    try {
+      await customer(payload);
+    } catch (error) {
+      expect(error.message).toMatch(/maximum of 500/);
+      expect(error.code).toEqual(ApiV3.ErrorCode.BatchLimitExceeded);
+    }
+  });
+});
