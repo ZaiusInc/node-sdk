53e41cb8324f5ff02b9ffa74f231ca9af11f626f
mejmt <john@zaius.com>
2019-07-30 17:03:37 -0400

    ZAIUS-8120: support for creating custom objects via schema api

---
+++ src/Api/Schema/objects.ts
@@ -0,0 +1,52 @@
+import {ApiV3} from '../lib/ApiV3';
+import {ObjectDefinition} from '../Types';
+import {ApiObjectExistsError} from './ApiObjectExistsError';
+import {ApiSchemaValidationError} from './ApiSchemaValidationError';
+
+/**
+ * Create a custom Zaius object
+ * @param object the object to create
+ * @throws {ApiObjectExistsError} if the object name already exists
+ */
+export async function createObject(object: ObjectDefinition): Promise<ApiV3.HttpResponse> {
+  validateCreateObject(object);
+
+  try {
+    return await ApiV3.post(`/schema/objects`, object);
+  } catch (e) {
+    if (e instanceof ApiV3.HttpError && e.response) {
+      const invalids: InvalidDetail[] = e.response.data && e.response.data.detail && e.response.data.detail.invalids;
+      if (invalids && invalids.find((detail) => detail.field === 'name' && /^already used/.test(detail.reason))) {
+        throw new ApiObjectExistsError(e);
+      }
+    }
+    throw e;
+  }
+}
+
+interface InvalidDetail {
+  field: string;
+  reason: string;
+}
+
+/**
+ * @hidden
+ * Temporary validation until we update milton
+ */
+function validateCreateObject(object: ObjectDefinition) {
+  const context = ApiV3.getAppContext();
+  if (context && context.app_id) {
+    const prefix = `${context.app_id}_`;
+    if (!object.name.startsWith(prefix)) {
+      throw new ApiSchemaValidationError(`object name ${object.name} must be prefixed with ${prefix}`);
+    }
+    if (object.alias && !object.alias.startsWith(prefix)) {
+      throw new ApiSchemaValidationError(`object alias ${object.name} must be prefixed with ${prefix}`);
+    }
+    if (!object.display_name.startsWith(context.display_name)) {
+      throw new ApiSchemaValidationError(
+        `object display name ${object.display_name} must be prefixed with ${context.display_name}`
+      );
+    }
+  }
+}
