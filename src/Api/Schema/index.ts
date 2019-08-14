90d02c476937b74651fbc2dce67d2970066b8acc
Levi Lansing <helives@gmail.com>
2019-08-14 15:06:14 -0400

    add missing exports

--- src/Api/Schema/index.ts
+++ src/Api/Schema/index.ts
@@ -1,4 +1,6 @@
 export * from './ApiFieldExistsError';
+export * from './ApiObjectExistsError';
+export * from './ApiRelationExistsError';
 export * from './ApiSchemaValidationError';
 export * from './fields';
 export * from './objects';
